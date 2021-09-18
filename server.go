2e1c21b49c2b69ae2624c7d3d7d08a1deea18377
Sarun Tapee <tpzrun@gmail.com>
2021-09-19 00:11:49 +0700

    comment bootcamp radius route

--- server.go
+++ server.go
@@ -23,21 +23,24 @@ func main() {
 
 	// bootcamp router
 	bc := controllers.NewBootcamp(conn)
-	// r.GET("/api/v1/bootcamps", bc.GetBootcamps)
-	// r.GET("/api/v1/bootcamps/:id", bc.GetBootcamp)
-	r.GET("/api/v1/bootcamps/radius/:zipcode/:distance", bc.GetBootcampsInRadius)
+	r.GET("/api/v1/bootcamps", bc.GetBootcamps)
+	r.GET("/api/v1/bootcamps/:id", bc.GetBootcamp)
+	/*
+	 * route's name conflicts
+	 */
+	// r.GET("/api/v1/bootcamps/radius/:zipcode/:distance", bc.GetBootcampsInRadius)
 	r.POST("/api/v1/bootcamps", bc.CreateBootcamp)
-	// r.PUT("/api/v1/bootcamps/:id", bc.UpdateBootcamp)
-	// r.DELETE("/api/v1/bootcamps/:id", bc.DeleteBootcamp)
+	r.PUT("/api/v1/bootcamps/:id", bc.UpdateBootcamp)
+	r.DELETE("/api/v1/bootcamps/:id", bc.DeleteBootcamp)
 
 	// course router
-	// c := controllers.NewCourse(conn)
-	// r.GET("/api/v1/courses", c.GetCourses)
-	// r.GET("/api/v1/bootcamps/:id/courses", c.GetCoursesInBootcamp)
-	// r.GET("/api/v1/courses/:id", c.GetCourse)
-	// r.POST("/api/v1/bootcamps/:id/courses", c.AddCourse)
-	// r.PUT("/api/v1/courses/:id", c.UpdateCourse)
-	// r.DELETE("/api/v1/courses/:id", c.DeleteCourse)
+	c := controllers.NewCourse(conn)
+	r.GET("/api/v1/courses", c.GetCourses)
+	r.GET("/api/v1/bootcamps/:id/courses", c.GetCoursesInBootcamp)
+	r.GET("/api/v1/courses/:id", c.GetCourse)
+	r.POST("/api/v1/bootcamps/:id/courses", c.AddCourse)
+	r.PUT("/api/v1/courses/:id", c.UpdateCourse)
+	r.DELETE("/api/v1/courses/:id", c.DeleteCourse)
 
 	port := os.Getenv("PORT")
 	port = fmt.Sprint(":", port)
