39bb9e358ef59bfa90a7a0e6799456039f47d5ae
Sarun Tapee <tpzrun@gmail.com>
2021-09-21 14:11:14 +0700

    implement review routes

--- controllers/review.go
+++ controllers/review.go
@@ -1,12 +1,16 @@
 package controllers
 
 import (
+	"devcamper/models"
 	"devcamper/utils"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
 	"github.com/zebresel-com/mongodm"
+	"gopkg.in/mgo.v2/bson"
 )
 
 type Review struct {
@@ -23,9 +27,19 @@ func NewReview(conn *mongodm.Connection) *Review {
 // @route   GET /api/v1/reviews
 // @access  Public
 func (rw *Review) GetReviews(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	Review := rw.connection.Model("Review")
+	reviews := []*models.Review{}
+
+	err := Review.Find(bson.M{"deleted": false}).Exec(&reviews)
+	if err != nil {
+		utils.ErrorResponse(w, http.StatusInternalServerError, errors.New("server error"))
+		return
+	}
+
 	utils.SendJSON(w, http.StatusOK, map[string]interface{}{
 		"success": true,
-		"data":    "get all reviews",
+		"count":   len(reviews),
+		"data":    reviews,
 	})
 }
 
@@ -33,10 +47,44 @@ func (rw *Review) GetReviews(w http.ResponseWriter, r *http.Request, ps httprout
 // @route   GET /api/v1/bootcamps/:id/reviews
 // @access  Public
 func (rw *Review) GetReviewsInBootcamp(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	id := ps.ByName("id")
+	bootcampId := ps.ByName("id")
+	if !bson.IsObjectIdHex(bootcampId) {
+		utils.ErrorResponse(w, http.StatusBadRequest, errors.New("invalid bootcamp id format"))
+		return
+	}
+
+	Bootcamp := rw.connection.Model("Bootcamp")
+	bootcamp := &models.Bootcamp{}
+	err := Bootcamp.FindId(bson.ObjectIdHex(bootcampId)).Exec(bootcamp)
+	if _, ok := err.(*mongodm.NotFoundError); ok {
+		utils.ErrorResponse(w, http.StatusBadRequest, fmt.Errorf("not found bootcamp with id of %s", bootcampId))
+		return
+	} else if err != nil {
+		utils.ErrorResponse(w, http.StatusInternalServerError, errors.New("server error"))
+		return
+	}
+	if bootcamp.Deleted {
+		utils.ErrorResponse(w, http.StatusNotFound, fmt.Errorf("the bootcamp with the id of %s was deleted", bootcampId))
+		return
+	}
+
+	Review := rw.connection.Model("Review")
+	reviews := []*models.Course{}
+
+	query := bson.M{
+		"bootcamp": bson.ObjectIdHex(bootcampId),
+		"deleted":  false,
+	}
+	err = Review.Find(query).Exec(&reviews)
+	if err != nil {
+		utils.ErrorResponse(w, http.StatusInternalServerError, errors.New("server error"))
+		return
+	}
+
 	utils.SendJSON(w, http.StatusOK, map[string]interface{}{
 		"success": true,
-		"data":    fmt.Sprintf("get all reviews in bootcamp id %s", id),
+		"count":   len(reviews),
+		"data":    reviews,
 	})
 }
 
@@ -45,9 +93,30 @@ func (rw *Review) GetReviewsInBootcamp(w http.ResponseWriter, r *http.Request, p
 // @access  Public
 func (rw *Review) GetReview(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	id := ps.ByName("id")
+	if !bson.IsObjectIdHex(id) {
+		utils.ErrorResponse(w, http.StatusBadRequest, errors.New("invalid review id format"))
+		return
+	}
+
+	Review := rw.connection.Model("Review")
+	review := &models.Review{}
+
+	err := Review.FindId(bson.ObjectIdHex(id)).Exec(review)
+	if _, ok := err.(*mongodm.NotFoundError); ok {
+		utils.ErrorResponse(w, http.StatusBadRequest, fmt.Errorf("not found review with id of %s", id))
+		return
+	} else if err != nil {
+		utils.ErrorResponse(w, http.StatusInternalServerError, errors.New("server error"))
+		return
+	}
+	if review.Deleted {
+		utils.ErrorResponse(w, http.StatusNotFound, errors.New("this review was deleted"))
+		return
+	}
+
 	utils.SendJSON(w, http.StatusOK, map[string]interface{}{
 		"success": true,
-		"data":    fmt.Sprintf("get review id %s", id),
+		"data":    review,
 	})
 }
 
@@ -55,10 +124,53 @@ func (rw *Review) GetReview(w http.ResponseWriter, r *http.Request, ps httproute
 // @route   GET /api/v1/bootcamps/:id/reviews
 // @access  Private
 func (rw *Review) AddReview(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	id := ps.ByName("id")
-	utils.SendJSON(w, http.StatusOK, map[string]interface{}{
+	cUser := getCurrentUser(rw.connection, r)
+	if cUser == nil {
+		utils.ErrorResponse(w, http.StatusUnauthorized, errors.New("unauthorized"))
+		return
+	}
+	if !cUser.IsUserInRoles("user", "admin") {
+		utils.ErrorResponse(w, http.StatusForbidden, fmt.Errorf("user with %s role do not autorize for this route", cUser.Role))
+		return
+	}
+
+	bootcampId := ps.ByName("id")
+	if !bson.IsObjectIdHex(bootcampId) {
+		utils.ErrorResponse(w, http.StatusBadRequest, errors.New("invalid bootcamp id format"))
+		return
+	}
+
+	Bootcamp := rw.connection.Model("Bootcamp")
+	bootcamp := &models.Bootcamp{}
+	err := Bootcamp.FindId(bson.ObjectIdHex(bootcampId)).Exec(bootcamp)
+	if _, ok := err.(*mongodm.NotFoundError); ok {
+		utils.ErrorResponse(w, http.StatusBadRequest, fmt.Errorf("not found bootcamp with id of %s", bootcampId))
+		return
+	} else if err != nil {
+		utils.ErrorResponse(w, http.StatusInternalServerError, errors.New("server error"))
+		return
+	}
+	if bootcamp.Deleted {
+		utils.ErrorResponse(w, http.StatusNotFound, fmt.Errorf("the bootcamp with the id of %s was deleted", bootcampId))
+		return
+	}
+
+	Review := rw.connection.Model("Review")
+	review := &models.Review{}
+	Review.New(review)
+
+	json.NewDecoder(r.Body).Decode(review)
+	review.Bootcamp = bson.ObjectIdHex(bootcampId)
+	review.User = cUser.Id
+	if valid, issue := review.ValidateCreate(); !valid {
+		utils.ErrorResponse(w, http.StatusBadRequest, issue...)
+		return
+	}
+	review.Save()
+
+	utils.SendJSON(w, http.StatusCreated, map[string]interface{}{
 		"success": true,
-		"data":    fmt.Sprintf("add review for bootcamp id %s", id),
+		"data":    review,
 	})
 }
 
@@ -66,10 +178,67 @@ func (rw *Review) AddReview(w http.ResponseWriter, r *http.Request, ps httproute
 // @route   PUT /api/v1/reviews/:id
 // @access  Private
 func (rw *Review) UpdateReview(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	cUser := getCurrentUser(rw.connection, r)
+	if cUser == nil {
+		utils.ErrorResponse(w, http.StatusUnauthorized, errors.New("unauthorized"))
+		return
+	}
+	if !cUser.IsUserInRoles("user", "admin") {
+		utils.ErrorResponse(w, http.StatusForbidden, fmt.Errorf("user with %s role do not autorize for this route", cUser.Role))
+		return
+	}
+
 	id := ps.ByName("id")
+	if !bson.IsObjectIdHex(id) {
+		utils.ErrorResponse(w, http.StatusBadRequest, errors.New("invalid review id format"))
+		return
+	}
+
+	Review := rw.connection.Model("Review")
+	review := &models.Review{}
+
+	err := Review.FindId(bson.ObjectIdHex(id)).Exec(review)
+	if _, ok := err.(*mongodm.NotFoundError); ok {
+		utils.ErrorResponse(w, http.StatusBadRequest, fmt.Errorf("not found review with id of %s", id))
+		return
+	} else if err != nil {
+		utils.ErrorResponse(w, http.StatusInternalServerError, errors.New("server error"))
+		return
+	}
+	if review.Deleted {
+		utils.ErrorResponse(w, http.StatusNotFound, errors.New("this review was deleted"))
+		return
+	}
+
+	if review.User != cUser.Id && cUser.Role != "admin" {
+		utils.ErrorResponse(w, http.StatusForbidden, errors.New("you do not have permission"))
+		return
+	}
+
+	var data map[string]interface{}
+	err = json.NewDecoder(r.Body).Decode(&data)
+	if err != nil {
+		utils.ErrorResponse(w, http.StatusBadRequest, errors.New("bad data"))
+		return
+	}
+	// delete unexpected field
+	delete(data, "bootcamp")
+	delete(data, "user")
+
+	// The Update method is incompleted so the error is not handled
+	// see https://github.com/zebresel-com/mongodm/issues/20
+	review.Update(data)
+
+	if valid, issues := review.ValidateUpdate(); !valid {
+		utils.ErrorResponse(w, http.StatusBadRequest, issues...)
+		return
+	}
+
+	review.Save()
+
 	utils.SendJSON(w, http.StatusOK, map[string]interface{}{
 		"success": true,
-		"data":    fmt.Sprintf("update review id %s", id),
+		"data":    review,
 	})
 }
 
@@ -77,9 +246,48 @@ func (rw *Review) UpdateReview(w http.ResponseWriter, r *http.Request, ps httpro
 // @route   DELETE /api/v1/reviews/:id
 // @access  Private
 func (rw *Review) DeleteReview(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	cUser := getCurrentUser(rw.connection, r)
+	if cUser == nil {
+		utils.ErrorResponse(w, http.StatusUnauthorized, errors.New("unauthorized"))
+		return
+	}
+	if !cUser.IsUserInRoles("user", "admin") {
+		utils.ErrorResponse(w, http.StatusForbidden, fmt.Errorf("user with %s role do not autorize for this route", cUser.Role))
+		return
+	}
+
 	id := ps.ByName("id")
+	if !bson.IsObjectIdHex(id) {
+		utils.ErrorResponse(w, http.StatusBadRequest, errors.New("invalid course id format"))
+		return
+	}
+
+	Review := rw.connection.Model("Review")
+	review := &models.Review{}
+
+	err := Review.FindId(bson.ObjectIdHex(id)).Exec(review)
+	if _, ok := err.(*mongodm.NotFoundError); ok {
+		utils.ErrorResponse(w, http.StatusNotFound, fmt.Errorf("no review with id of %s", id))
+		return
+	} else if err != nil {
+		utils.ErrorResponse(w, http.StatusInternalServerError, errors.New("server error"))
+		return
+	}
+	if review.Deleted {
+		// should not found the deleted course
+		utils.ErrorResponse(w, http.StatusNotFound, fmt.Errorf("no review with id of %s", id))
+		return
+	}
+
+	if review.User != cUser.Id && cUser.Role != "admin" {
+		utils.ErrorResponse(w, http.StatusForbidden, errors.New("you do not have permission"))
+		return
+	}
+
+	review.SetDeleted(true)
+	review.Save()
 	utils.SendJSON(w, http.StatusOK, map[string]interface{}{
 		"success": true,
-		"data":    fmt.Sprintf("delete review id %s", id),
+		"data":    nil,
 	})
 }
